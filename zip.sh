fa6178297159b4b87bdecd7427eac3d3f9bbe9da
afnleaf <afnleafow@gmail.com>
2025-08-01 00:54:18 -0400

    Added: unified codebase

--- zip.sh
+++ zip.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# Get the directories
-dirBlink=$(dirname "$0")/Blink
-dirGecko=$(dirname "$0")/Gecko
-
-# Filenames
-zipfileBlink="pokepastefix_blink.zip"
-zipfileGecko="pokepastefix_gecko.zip"
-
+# script to zip extension package from folder contents
+# get the location, set output file name
 # navigate to dir and zip all files in there
-cd "$dirBlink"
-zip -r "../$zipfileBlink" .
 
-# cd out
+dirExtension=$(dirname "$0")/Extension
+
+zipfileExtension="pokepastefix_pkg.zip"
+
+cd "$dirExtension"
+zip -r "../$zipfileExtension" .
 cd ..
 
-# again
-cd "$dirGecko"
-zip -r "../$zipfileGecko" .
-
-cd ..
